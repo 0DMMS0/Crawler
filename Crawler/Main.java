c3b05f5642f09d4b1ea016a45b48ba0ce942262b
mauricio-bright <mauricio.mora@brightsec.com>
2025-12-29 21:43:53 -0600

    adding logic

--- Crawler/Main.java
+++ Crawler/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo desde Java");
+public class Main { 
+	public static void main(String[] args) {    
+		String url = "https://example.com";
+		int maxPages = 5;
+	
+		System.out.println("Initial URL: " + url);
+		System.out.println("Maximun Pages: " + maxPages);
+
+		for(int i = 1; i <= 3; i++) {
+			System.out.println("On page #:" + i);
+
+		}
     }
 }
